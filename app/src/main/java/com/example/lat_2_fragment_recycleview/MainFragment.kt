f3f1c9e1f6c1cdb93efa0879b9cdbe1748ecfe58
Alfonsdev <106644852+AlfonsInfo@users.noreply.github.com>
2023-03-09 23:51:07 +0700

    recycleview di  fragment

--- app/src/main/java/com/example/lat_2_fragment_recycleview/MainFragment.kt
+++ app/src/main/java/com/example/lat_2_fragment_recycleview/MainFragment.kt
@@ -1,41 +1,43 @@
 package com.example.lat_2_fragment_recycleview
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.lat_2_fragment_recycleview.entity.Mahasiswa
 
-class MainActivity : AppCompatActivity() {
 
+class MainFragment : Fragment() {
 
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        //Menampilkan view
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        getSupportFragmentManager().beginTransaction().replace(R.id.layout_fragment, MainFragment()).commit()
-
-
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-    fun bindingRecycleViewonActivity()
-    {
         //Set layout manager ke konteks. Layout Manager : Mengatur bagaimana kita menampilkan data, secara vertikal ?
         //ada 2 kolomkah ? atau secara horizontal ?
-        val layoutManager = LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false)
+        val layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL,false)
 
         // variable konstan adapter, yang nilainya merupakan objek dari class RVMahasiswaAdapter, dengan parameter companion object dari Mahasiswa
         val adapter =  RVMahasiswaAdapter(Mahasiswa.listOfMahasiswa)
 
 
         // Menghubungkan penamoungnya di activity_main dengan RecycleView
-        val rvMahasiswa : RecyclerView = findViewById(R.id.rv_mahasiswa)
+        val rvMahasiswa : RecyclerView = view.findViewById(R.id.rv_mahasiswa)
 
         rvMahasiswa.layoutManager = layoutManager // setting layoutnya hori/verti/tabel?
         rvMahasiswa.setHasFixedSize(true)
         rvMahasiswa.adapter = adapter // menghubungkan dengan adapter(binding value)
+
     }
+
 }
