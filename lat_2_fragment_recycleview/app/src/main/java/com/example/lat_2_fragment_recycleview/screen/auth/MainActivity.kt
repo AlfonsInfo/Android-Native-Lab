a5448cb4f22575b0da6e4e29ec2930a69a19a6cc
Alfonsdev <106644852+AlfonsInfo@users.noreply.github.com>
2023-03-10 13:38:29 +0700

    navigasi part 1

--- lat_2_fragment_recycleview/app/src/main/java/com/example/lat_2_fragment_recycleview/screen/auth/MainActivity.kt
+++ lat_2_fragment_recycleview/app/src/main/java/com/example/lat_2_fragment_recycleview/screen/auth/MainActivity.kt
@@ -2,17 +2,25 @@ package com.example.lat_2_fragment_recycleview.screen.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.fragment.app.FragmentTransaction
 import com.example.lat_2_fragment_recycleview.R
 
 //Next Navigasi (:)(:)
 class MainActivity : AppCompatActivity() {
 
 
-
     override fun onCreate( savedInstanceState: Bundle?) {
         //Menampilkan view
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        getSupportFragmentManager()
+            .beginTransaction()
+            .replace(R.id.layout_fragment , LoginFragment())
+            .setCustomAnimations(com.google.android.material.R.anim.abc_fade_out,
+                androidx.appcompat.R.anim.abc_fade_in)
+            .commit()
 
 
 
--- lat_2_fragment_recycleview/app/src/main/java/com/example/lat_2_fragment_recycleview/screen/home/SecondActivity.kt
+++ lat_2_fragment_recycleview/app/src/main/java/com/example/lat_2_fragment_recycleview/screen/home/SecondActivity.kt
@@ -3,10 +3,18 @@ package com.example.lat_2_fragment_recycleview.screen.home
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.lat_2_fragment_recycleview.R
+import com.example.lat_2_fragment_recycleview.screen.auth.LoginFragment
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        getSupportFragmentManager()
+            .beginTransaction()
+            .replace(R.id.layout_fragment , HomeFragment())
+            .setCustomAnimations(com.google.android.material.R.anim.abc_fade_out,
+                androidx.appcompat.R.anim.abc_fade_in)
+            .commit()
     }
 }
